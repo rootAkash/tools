3bf46a6bbed069d8973269acf13eb515efd397dc
rootAkash <mr.akashraut@gmail.com>
2023-03-28 18:25:19 +0530

    added edit feature

--- pages/LJspeechTool.py
+++ pages/LJspeechTool.py
@@ -1,9 +1,14 @@
 from utils.LJdataReader import read_csv_LJspeech,read_audios
 import streamlit as st
 
+
 def run_LJpage():
+    def change_phonemes():
+        st.session_state["editedPhoneme"] = st.session_state["editedtranscript"]
     if "a_counter" not in st.session_state:
         st.session_state["a_counter"]=0
+    if "enable_edit" not in st.session_state:
+        st.session_state["enable_edit"]=False
     path_to_csv = st.text_input('MetaData CSV file path')
     metaData = None
     audioData = None
@@ -21,15 +26,30 @@ def run_LJpage():
         if prev_press:
             if st.session_state["a_counter"]>0:
                 st.session_state["a_counter"]-=1
+                st.session_state["enable_edit"]=False
         if next_press:
             if st.session_state["a_counter"]<len(metaData["files"])-1:
                 st.session_state["a_counter"]+=1
+                st.session_state["enable_edit"]=False
         audio_bytes = audioData[metaData["files"][st.session_state["a_counter"]]]
         st.audio(audio_bytes, format='audio/wav')#st.audio(data, format='audio/wav', start_time=0)
         st.text("transcripts:")
         st.write(metaData["transcripts"][st.session_state["a_counter"]])
+        edit = st.button("edit")
+        if edit:
+            st.session_state["enable_edit"]= not  st.session_state["enable_edit"]
+        if st.session_state["enable_edit"]:
+            new_transcript = st.text_area("enter new transcript:",metaData["transcripts"][st.session_state["a_counter"]],key="editedtranscript",
+                                          on_change=change_phonemes)
+            #call function to edit it in csv when apply button is pressed
+
+
         st.text("phoneme:")
         st.write(metaData["phonemes"][st.session_state["a_counter"]])
+        if st.session_state["enable_edit"]:
+            new_phoneme = st.text_area("new phoneme is same as transcription by default:",metaData["phonemes"][st.session_state["a_counter"]],key="editedPhoneme")
+            #call function to save new phoneme when apply is clicked
+
         st.text("file:")
         st.write(metaData["files"][st.session_state["a_counter"]])
         st.text("current counter:" + str(st.session_state["a_counter"]))
